1c728318f16281602629d694b8cf8fb193586635
Ivan <vanya1254@yandex.ru>
2023-07-29 14:03:30 +0300

    structure

--- src/model/animal/pet/Hamster.java
+++ src/model/animal/pet/Hamster.java
@@ -2,32 +2,16 @@ package model.animal.pet;
 
 import model.animal.Pet;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Hamster extends Pet {
-    private List<String> commands;
 
-    public Hamster(String name, String residence, String howUsed) {
-        super(name, residence, howUsed);
-        commands = new ArrayList<>();
+    public Hamster(String name, String residence, String howUsed, List<String> commands) {
+        super(name, residence, howUsed, commands);
     }
 
     @Override
     public String getAnimalType() {
         return "Hamster";
-    }
-
-    @Override
-    public void showCommands() {
-        System.out.println(getName() + " knows the following commands: ");
-        for (String command : commands) {
-            System.out.println("- " + command);
-        }
-    }
-
-    @Override
-    public void teachCommand(String command) {
-        commands.add(command);
     }
 }
